e4d28352703e6cb4533daeb2ce5d469c907a7f3c
Cyber MacGeddon <cybermaggedon@gmail.com>
2021-05-18 20:35:26 +0100

    Packaging for PyPI

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gnucash-ixbrl",
-    version="1.1",
+    version="1.0",
     author="Cybermaggedon",
     author_email="mark@cyberapocalypse.co.uk",
-    description="Present GnuCash account in UK-style form including iXBRL",
+    description="Production of iXBRL reports from GnuCash accounts",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cybermaggedon/gnucash-ixbrl",
@@ -19,7 +19,9 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    download_url = "https://github.com/cybermaggedon/gnucash-ixbrl/archive/refs/tags/v1.0.tar.gz",
     install_requires=[
+        "requests"
     ],
     scripts=[
         "scripts/gnucash-ixbrl"
