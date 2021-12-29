cybermaggedon/gnucash-ixbrl

Basic CSV account input

gnucash_ixbrl/accounts.py
@@ -6,6 +6,9 @@ def get_class(kind):
     elif kind == "piecash":
         import gnucash_ixbrl.accounts_piecash as a
         return a.Accounts
+    elif kind == "csv":
+        import gnucash_ixbrl.accounts_csv as a
+        return a.Accounts
     else:
         raise RuntimeError("Accounts kind '%s' not known" % kind)
 

cybermaggedon/ixbrl-reporter

ixbrl_reporter/accounts.py
@@ -6,6 +6,9 @@ def get_class(kind):
     elif kind == "piecash":
         import ixbrl_reporter.accounts_piecash as a
         return a.Accounts
+    elif kind == "csv":
+        import ixbrl_reporter.accounts_csv as a
+        return a.Accounts
     else:
         raise RuntimeError("Accounts kind '%s' not known" % kind)
 