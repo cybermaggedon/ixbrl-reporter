e67b120e0989767e5ee9a7672a2f592cfad78467
Cyber MacGeddon <cybermaggedon@gmail.com>
2024-09-10 00:03:59 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ixbrl-reporter",
-    version="1.1.0",
+    version="1.1.1",
     author="Cybermaggedon",
     author_email="mark@cyberapocalypse.co.uk",
     description="Production of iXBRL reports from templates and accounts files",
